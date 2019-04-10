d439e709bda572fa8def852b72f42b5ff72bc7dd
Varsha-Arun <varsha@shippable.com>
2019-04-10 14:52:41 +0530

    Adding tests for resource version API

--- tests/core/api/PIPELINESOURCES.js
+++ tests/core/api/PIPELINESOURCES.js
@@ -55,7 +55,7 @@ describe(test,
         var body = {
          "masterIntegrationId": 20,
          "masterIntegrationName": "github",
-         "name": 'git42',
+         "name": 'githubIntegration',
          "projectId": project.id,
          "formJSONValues": [
              {
